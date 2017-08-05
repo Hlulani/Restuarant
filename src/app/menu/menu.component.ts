230031f2369df36f5dd669bf93bb5a24d0611fde
marvin-rao <30743056+marvin@users.noreply.github.com>
2017-08-05 18:12:00 +0200

    Hi Hlulani

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,16 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
-
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+  menus: FirebaseListObservable<any[]>;
+  constructor(db: AngularFireDatabase, private router: Router) {
+    this.menus = db.list('resturents/hrll3/menus/Beverages');
+  }
 }
+
+
