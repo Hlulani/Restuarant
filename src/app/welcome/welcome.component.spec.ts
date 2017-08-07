21fd000fab83009e8bd91e0fe2ffa6502618058c
Hlulani Baloyi <hlulani.baloyi@dhub.co.za>
2017-08-07 23:20:34 +0200

    [Hlulani] Changes made onthe Menu component,replaced hardcoded data with one from the database

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -1,42 +1,52 @@
-import {WelcomeComponent} from './welcome.component';
-import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
-import {Router} from '@angular/router';
-import {By} from '@angular/platform-browser';
+
+import { async, fakeAsync, TestBed} from '@angular/core/testing';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { WelcomeComponent } from './welcome.component';
+import { ComponentFixture } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
+import { By } from '@angular/platform-browser';
+
 
 describe('welcome component', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
-  let nextButton: HTMLButtonElement;
+  let nextButton: HTMLBodyElement;
   const mockRouter = {
     navigate: jasmine.createSpy('navigate')
   };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [],
       declarations: [WelcomeComponent],
       providers: [
-        { provide: Router, useValue: mockRouter },
+        { provide: Router, useValue: mockRouter }
       ]
     }).compileComponents();
   }));
 
-  beforeEach(() => {
+beforeEach(() => {
     fixture = TestBed.createComponent(WelcomeComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    component = fixture.componentInstance;
 
+  });
   it('should create a component', () => {
     expect(component).toBeTruthy();
   });
-   describe('when the place holder button is pressed', () => {
+
+  describe('when the place holder button pressed', () => {
     beforeEach(() => {
       nextButton = fixture.debugElement.query(By.css('#place-order')).nativeElement;
       nextButton.click();
+       fixture.detectChanges();
     });
 
-    it('should route to categiries', fakeAsync(() => {
+    it('should route to the categories component', fakeAsync(() => {
       expect(mockRouter.navigate).toHaveBeenCalledWith(['/categories']);
     }));
-
 });
+
 });
